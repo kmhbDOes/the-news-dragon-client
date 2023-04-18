68d2fe021ec51b9a29d63d29464e5e73096ff99a
kmhbDOes <kmhbipul@gmail.com>
2023-04-18 15:34:44 +0600

    left and right sidebar added

--- src/pages/Shared/RightNav/RightNav.jsx
+++ src/pages/Shared/RightNav/RightNav.jsx
@@ -1,9 +1,45 @@
+import { Button, ListGroup } from "react-bootstrap";
 import React from "react";
+import {
+  FaGoogle,
+  FaGithubSquare,
+  FaFacebook,
+  FaTwitter,
+  FaInstagram,
+} from "react-icons/fa";
+import QZone from "../QZone/QZone";
+import bg from "../../../assets/bg.png";
 
 const RightNav = () => {
   return (
     <div>
-      <h2>RightNav</h2>
+      <h4>Login With</h4>
+      <Button className="mb-2" variant="outline-primary">
+        <FaGoogle />
+        Login With Google
+      </Button>
+      <Button variant="outline-primary">
+        <FaGithubSquare />
+        Login With Github
+      </Button>
+      <div>
+        <h4>Find Us On</h4>
+        <ListGroup>
+          <ListGroup.Item>
+            <FaFacebook></FaFacebook>Facebook
+          </ListGroup.Item>
+          <ListGroup.Item>
+            <FaTwitter></FaTwitter>Twitter
+          </ListGroup.Item>
+          <ListGroup.Item>
+            <FaInstagram></FaInstagram>Instagram
+          </ListGroup.Item>
+        </ListGroup>
+      </div>
+      <QZone></QZone>
+      <div>
+        <img src={bg} alt="" />
+      </div>
     </div>
   );
 };
