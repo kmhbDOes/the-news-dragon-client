17be38c8ff28ba41f3e2a8529546ac7463354018
kmhbDOes <kmhbipul@gmail.com>
2023-04-18 13:23:44 +0600

    Marquee added

--- src/pages/Shared/Header/Header.jsx
+++ src/pages/Shared/Header/Header.jsx
@@ -1,16 +1,25 @@
 import React from "react";
 import logo from "../../../assets/logo.png";
 import moment from "moment";
+import { Button } from "react-bootstrap";
+import { Container } from "react-bootstrap";
+import Marquee from "react-fast-marquee";
 
 const Header = () => {
   return (
-    <div className="text-center">
-      <img src={logo} alt="" />
-      <p className="text-secondary">
-        <small>Journalism Without Fear or Favor</small>
-      </p>
-      <p>{moment().format("dddd, MMMM Do YYYY")}</p>
-    </div>
+    <Container>
+      <div className="text-center">
+        <img src={logo} alt="" />
+        <p className="text-secondary">
+          <small>Journalism Without Fear or Favor</small>
+        </p>
+        <p>{moment().format("dddd, MMMM Do YYYY")}</p>
+      </div>
+      <div>
+        <Button variant="danger">Latest</Button>
+        <Marquee>I can be a react component</Marquee>
+      </div>
+    </Container>
   );
 };
 
