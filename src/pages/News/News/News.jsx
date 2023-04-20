624388233e621ac4ee91e6564f9d3a22f89912ce
kmhbDOes <kmhbipul@gmail.com>
2023-04-21 03:13:14 +0600

    Editors Picks added

--- src/pages/News/News/News.jsx
+++ src/pages/News/News/News.jsx
@@ -1,9 +1,24 @@
 import React from "react";
+import { Button, Card } from "react-bootstrap";
+import { Link, useLoaderData } from "react-router-dom";
+import EditorsPicks from "../EditorsPick/EditorsPick";
 
 const News = () => {
+  const news = useLoaderData();
+  const { _id, title, details, image_url, category_id } = news;
   return (
     <div>
-      <h2>News details coming soon</h2>
+      <Card>
+        <Card.Img variant="top" src={image_url} />
+        <Card.Body>
+          <Card.Title>{title}</Card.Title>
+          <Card.Text> {details} </Card.Text>
+          <Link to={`/category/${category_id}`}>
+            <Button variant="danger">All News In This Category</Button>
+          </Link>
+        </Card.Body>
+      </Card>
+      <EditorsPicks></EditorsPicks>
     </div>
   );
 };
